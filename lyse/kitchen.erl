a3855f290301b201ca39655ac4f0eb9d0cf88242
Philip Stears <philip@driveworks.co.uk>
2014-09-18 21:11:44 +0100

    kitchen

---
+++ lyse/kitchen.erl
@@ -0,0 +1,32 @@
+-module(kitchen).
+-compile(export_all).
+
+fridge1() ->
+    receive
+        {From, {store, Food}} ->
+            From ! {self(), ok},
+            fridge1();
+        {From, {take, Food}} ->
+            From ! {self(), not_found},
+            fridge1();
+        terminate ->
+            ok
+    end.
+
+fridge2(FoodList) ->
+    receive
+        {From, {store, Food}} ->
+            From ! {self(), ok},
+            fridge2([Food|FoodList]);
+        {From, {take, Food}} ->
+            NewFoodList = lists:delete(Food, FoodList),
+            TookSomething = NewFoodList /= FoodList,
+            Return = case TookSomething of
+                         true -> ok;
+                         false -> not_found
+                     end,
+            From ! {self(), Return},
+            fridge2(NewFoodList);
+        terminate ->
+            ok
+    end.
